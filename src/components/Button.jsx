b3fde042d819b0f31f1a6e26bde33a61856a6d1c
jyo <bosejyotiska@gmail.com>
2024-06-11 22:07:07 +0530

    add components

---
+++ src/components/Button.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+// this is a common button and we'll use it for all buttons instead of making one every time
+
+
+
+export default function Button({
+    children,
+    type = "button",
+    bgColor = "bg-blue-600",
+    textColor = "text-white",
+    className = "",
+    ...props
+}) {
+    return (
+        <button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`} {...props}>
+            {children}
+        </button>
+    );
+}
