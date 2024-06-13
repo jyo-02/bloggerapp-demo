fdb1726348207227cbfd711f715b02852ca53ff0
jyo <bosejyotiska@gmail.com>
2024-06-13 22:03:59 +0530

    small fixes

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,21 +1,19 @@
 import React from "react";
-//real Time editor creating this component without forwardingRef hook. here we are learning how to stay updated state of this hook through react hook form
 import { Editor } from "@tinymce/tinymce-react";
 import { Controller } from "react-hook-form";
 
 export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
     <div className="w-full">
-      {label && <label className="inline-block mb- pl-1"> {label}</label>}
+      {label && <label className="inline-block mb- pl-1">{label}</label>}
       <Controller
         name={name || "content"}
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
-            initialValue={defaultValue}
+            initialValue={defaultValue} // Set initial value here
             apiKey="vs6cmeq2lmfm0c68xqbce7wli3sbh477rpqcatd867hy0swf"
             init={{
-              initialValue: defaultValue,
               height: 500,
               menubar: true,
               plugins: [
