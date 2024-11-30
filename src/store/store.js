e2f3906e3d2e367181db2ed1637a0a698dff96bd
jyo <bosejyotiska@gmail.com>
2024-11-30 23:19:10 +0530

    first commit

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,1 @@
-import {configureStore} from '@reduxjs/toolkit'
-import authSlice from './authSlice';
-
-
-const store = configureStore({
-    reducer : {
-        auth : authSlice,
-    }
-});
-
-export default store;
+//code removed contact for full code
