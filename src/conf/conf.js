c73850e35185ed9acf35fc850ccf6c8dd46c1982
Jyotiska <169176202+jyo-02@users.noreply.github.com>
2025-03-29 15:39:21 +0530

    Update conf.js

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,9 +1,2 @@
-const conf = {
-    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
-    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-}
-// We're doing this to ensure that the environment variables are always in string format, to prevent the app from crashing while using int variables 
+const conf = {lmao}
 export default conf
